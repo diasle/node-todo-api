7b11d95475e1ba7900f88d2766ddd01496b987dd
Diana Sleczek <dsleczek@egnyte.com>
2018-11-26 21:11:33 +0100

    GET todos test added

--- server/server.spec.js
+++ server/server.spec.js
@@ -4,8 +4,12 @@ const request = require("supertest");
 const { app } = require("./server");
 const { Todo } = require("./models/Todo");
 
+const todos =  [{ text: "First" }, { text: "Second" }];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe("POST/todos", () => {
@@ -26,8 +30,8 @@ describe("POST/todos", () => {
 
                Todo.find()
                    .then((todos) => {
-                    expect(todos.length).toBe(1);
-                    expect(todos[0].text).toBe(text);
+                    expect(todos.length).toBe(3);
+                    expect(todos[2].text).toBe(text);
 
                    done();
                })
@@ -47,11 +51,23 @@ describe("POST/todos", () => {
 
                 Todo.find()
                     .then((todos) => {
-                        expect(todos.length).toBe(0);
+                        expect(todos.length).toBe(2);
 
                         done();
                     })
                     .catch((e) => done(e))
             });
+    });
+});
+
+describe("GET/todos", () => {
+    it("gets all todos", (done) => {
+        request(app)
+            .get("/todos")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done);
     });
 });
