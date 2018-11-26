c704922bc09be7e77521f140f7c57b3273378f25
Diana Sleczek <dsleczek@egnyte.com>
2018-11-26 20:22:23 +0100

    Add post todos route and refactored moongose

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/Todo");
+const { User } = require("./models/User");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+    console.log(req.body);
+    const todo = new Todo({
+        text: req.body.text,
+    });
+
+    todo.save()
+        .then((doc) => {
+            res.send(doc);
+        })
+        .catch((e) => {
+            res.status(400).send(e);
+        });
+});
+
+app.get("/todos", (req, res) => {
+
+});
+
+app.del("/todos/id", (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(3000, () => {
+    console.log("Started on port 3000")
+});
