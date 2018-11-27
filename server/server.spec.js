0ed9b1f8577ad58653166550de3fe4289ada2f63
Diana Sleczek <dsleczek@egnyte.com>
2018-11-27 22:59:37 +0100

    Tests for GET/id

--- server/server.spec.js
+++ server/server.spec.js
@@ -1,10 +1,11 @@
 const expect = require("expect");
 const request = require("supertest");
+const { ObjectID } = require("mongodb");
 
 const { app } = require("./server");
 const { Todo } = require("./models/Todo");
 
-const todos =  [{ text: "First" }, { text: "Second" }];
+const todos =  [{ text: "First", _id: new ObjectID() }, { text: "Second", _id: new ObjectID() }];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
@@ -68,6 +69,34 @@ describe("GET/todos", () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2)
             })
+            .end(done);
+    });
+});
+
+describe("GET/todos/:id", () => {
+    it("get specific todo", (done) => {
+        request(app)
+            .get(`/todos/${ todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe( todos[0].text)
+            })
+            .end(done);
+    });
+
+    it("returns 404 when id is not valid", (done) => {
+        request(app)
+            .get("/todos/5bfc52c41")
+            .expect(404)
+            .end(done);
+    });
+
+    it("returns 404 when todo is not found", (done) => {
+        const customId = new ObjectID();
+
+        request(app)
+            .get(`/todos/${ customId.toHexString() }`)
+            .expect(404)
             .end(done);
     });
 });
